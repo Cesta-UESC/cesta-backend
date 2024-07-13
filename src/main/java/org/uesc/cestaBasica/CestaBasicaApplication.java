f0bfcad851fda4a65d1bb815c52388c3b19e078f
Joao-Moura <jaummoura15@gmail.com>
2024-07-13 12:20:26 -0300

    Iniciando estrutura do spring boot

---
+++ src/main/java/org/uesc/cestaBasica/CestaBasicaApplication.java
@@ -0,0 +1,13 @@
+package org.uesc.cestaBasica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CestaBasicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CestaBasicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uesc/cestaBasica/CestaBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uesc.cestaBasica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CestaBasicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
