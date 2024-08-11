05b437e33042c851164113f85ae4a6f79028baa3
Joao-Moura <jaummoura15@gmail.com>
2024-08-11 12:29:29 -0300

    reset project using go

--- src/main/java/org/uesc/cestaBasica/controllers/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package org.uesc.cestaBasica.controllers;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-}
--- src/main/java/org/uesc/cestaBasica/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.uesc.cestaBasica.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.uesc.cestaBasica.entities.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
